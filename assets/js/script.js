9d64fa5fb2e850be9ac33d9b2a030f64696de42c
Joao Netto <jnetto@fyyb.com.br>
2019-10-24 23:47:46 -0300

    updated css and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 if(document.getElementById('form-signup')) {
     let form = document.getElementById('form-signup');
+    let pwd = form.querySelector('input[name=pwd]');
+    let pwd2 = form.querySelector('input[name=pwd2]');
     let helpers = form.querySelectorAll('input.helper');
+    
     Array.prototype.forEach.call(helpers, helper => {
         helper.addEventListener('focus', () => {
             ((helper.parentNode).querySelector('small')).classList.remove('d-none');
@@ -9,15 +12,22 @@ if(document.getElementById('form-signup')) {
             ((helper.parentNode).querySelector('small')).classList.add('d-none');
         });
     });
-    
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        let pwd = form.querySelector('input[name=pwd]');
-        let pwd2 = form.querySelector('input[name=pwd2]');
-        if(pwd.value !== pwd2.value) {
-            pwd2.style.borderColor = 'red';
-            return;
+
+    pwd2.addEventListener('blur',() => {
+        if(pwd2.value !== pwd.value) {
+            if(!(pwd2.parentNode).classList.contains('has-error')) {
+                (pwd2.parentNode).classList.add('has-error');
+            };
+        } else {
+            if((pwd2.parentNode).classList.contains('has-error')) {
+                (pwd2.parentNode).classList.remove('has-error');
+            };
         };
+    });
+    
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        if((form.querySelectorAll('.has-error')).length > 0) return;
         form.submit();
-    })
+    });
 }
