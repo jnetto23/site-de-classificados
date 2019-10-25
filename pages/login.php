7c2a8974fcd70f3b5285b5f64d9b90207a06d84c
Joao Netto <jnetto@fyyb.com.br>
2019-10-25 00:22:47 -0300

    created (frontend) 'login'

---
+++ pages/login.php
@@ -0,0 +1,19 @@
+<?php require './pages/header.php'; ?>
+    <main class="container">
+    <form id="form-signin" method='POST'>
+            <span class="text-center text-uppercase h5">Login</span>
+
+            <div class="form-group">
+                <label class="mb-1" for="email">Email:</label>
+                <input class="form-control" type="email" name="email" placeholder="Informe seu Email" required>
+            </div>
+            <div class="form-group">
+                <label class="mb-1" for="pwd">Senha:</label>
+                <input class="form-control helper" type="password" name="pwd" placeholder="Informe sua senha" minlength="6" required>
+                <small class="d-none form-text text-muted">sua senha deve possuir no mínimo 6 caracteres.</small>
+            </div>
+            <input type="submit" class="btn btn-primary mt-2 mb-3" value="Entrar">
+            <span class="text-center">Ainda não tem cadastro? Então <a href="./cadastre-se">Cadastre-se</a></span>
+        </form>       
+    </main>
+<?php require './pages/footer.php'; ?>
