3e05f5713ee984eced2c7b08ac15988a21a3ea45
Joao Netto <jnetto@fyyb.com.br>
2019-10-24 22:49:24 -0300

    refactoring the project structure

---
+++ pages/index.php
@@ -0,0 +1,20 @@
+<?php require './pages/header.php'; ?>
+    <main>
+        <div class="jumbotron">
+            <div class="container">
+                <h2>Nós temos hoje 9999 anúncios.</h2>
+                <p>E mais de 9999 usuários cadastrados.</p>
+            </div>
+        </div>
+        <div class="container content">
+            <div class="row">
+                <div class="col-12 col-md-3">
+                    <h5>Pesquisa Avançada</h5>
+                </div>
+                <div class="col-12 col-md-9">
+                    <h5>Últimos Anúncios</h5>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php require './pages/footer.php'; ?>
