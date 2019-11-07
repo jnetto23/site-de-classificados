8596f416c9657b13c7828865687417a39b2ee42e
Joao Netto <jnetto@fyyb.com.br>
2019-11-06 23:50:59 -0300

    updated View and Controller (home)

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,13 +3,56 @@
 namespace Fyyb\Controllers;
 
 use \Fyyb\Core\Controller;
-use \Fyyb\Models\User;
+use \Fyyb\Models\{
+    User,
+    Ads,
+    Category
+};
 
 class HomeController extends Controller
 {
     public function index()
     {   
-        $data = array();
+        $user = new User();
+        $ads  = new Ads();
+        $cat  = new Category();
+
+        $filter = array(
+            'category' => '',
+            'value' => '',
+            'state' => '',
+            'img' => ''
+        );
+
+        if (isset($_GET['filter']) && !empty($_GET['filter'])) {
+            $filter = $_GET['filter'];
+        };
+
+        $nAds   = $ads->getTotal($filter);
+        $nUsers = $user->getTotal();
+        $c      = $cat->getList();
+        $p      = (isset($_GET['p']) && !empty($_GET['p'])) ? addslashes($_GET['p']) : 1;
+        $qtd    = 5;
+
+        $total_pages = ceil($nAds / $qtd);
+
+        $list = $ads->getList($p, $qtd, $filter);
+
+        $data = array(
+            'filter'      => $filter,
+            'p'           => $p,
+            'qtd'         => $qtd,
+            'total_pages' => $total_pages,
+            'list'        => $list,
+            'nAds'        => $nAds,
+            'nUsers'      => $nUsers,
+            'c'           => $c
+        );
         $this->loadViewInTemplate('home', $data, 'template');
+    }
+
+    public function ads($id)
+    {
+        echo 'Exibir Anuncio de ID '.$id;
     }
 }
