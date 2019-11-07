58a642fc49d3d16fbef3e8faf15428c0211fed62
Joao Netto <jnetto@fyyb.com.br>
2019-11-07 01:30:48 -0300

    updated routes

--- src/routes.php
+++ src/routes.php
@@ -10,6 +10,6 @@ $routes['/login']                 = '/auth/signin';
 $routes['/logout']                = '/auth/logout';
 
 $routes['/meus-anuncios']         = '/ads/index';
-$routes['/novo-anuncios']         = '/ads/add';
+$routes['/adicionar-anuncio']     = '/ads/add';
 $routes['/editar-anuncio/{id}']   = '/ads/edit/:id';
 $routes['/excluir-anuncio/{id}']  = '/ads/del/:id';
