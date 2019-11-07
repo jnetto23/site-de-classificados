9e1cfa1a0d984a6e7812aaa52c9444a4c7f19a42
Joao Netto <jnetto@fyyb.com.br>
2019-11-06 21:18:38 -0300

    updated project routes

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,13 @@
 
 global $routes;
 
-$routes['/'] = '/home/index';
-$routes['/contato'] = '/home/contato';
-$routes['/teste/{id}'] = '/home/teste/:id';
-$routes['/teste/{id}/{var}'] = '/home/teste/:id/:var';
+$routes['/']                    = '/home/index';
+
+$routes['/cadastre-se']         = '/auth/signup';
+$routes['/login']               = '/auth/signin';
+$routes['/logout']              = '/auth/logout';
+
+$routes['/meus-anuncios']       = '/ads/index';
+$routes['/add-anuncios']        = '/ads/add';
+$routes['/edit-anuncios/{id}']  = '/ads/edit/:id';
+$routes['/del-anuncios/{id}']   = '/ads/del/:id';
